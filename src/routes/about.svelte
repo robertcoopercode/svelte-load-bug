50e9a3af83bd5df92289f7d1b4ccb4b1cc8a94de
Robert Cooper <robert@robertcooper.me>
2022-03-24 23:44:05 -0400

    Initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,49 @@
+<script context="module">
+  import { todosStore } from "$lib/store";
+
+  export const load = async () => {
+    console.log("Loading about");
+
+    const waitTwoSeconds = async () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 2000);
+      });
+    };
+
+    todosStore.set(["four", "five", "six"]);
+
+    await waitTwoSeconds();
+
+    return {
+      props: {},
+    };
+  };
+</script>
+
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<section>
+  <h1>About</h1>
+
+  {#each $todosStore as todo}
+    <p>{todo}</p>
+  {/each}
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
+
+  h1 {
+    width: 100%;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<script context="module">
+  import { todosStore } from "$lib/store";
+
+  export const load = () => {
+    console.log("Loading home");
+    todosStore.set(["one", "two", "three"]);
+
+    return {
+      props: {},
+    };
+  };
+</script>
+
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<section>
+  <h1>Home</h1>
+
+  {#each $todosStore as todo}
+    <p>{todo}</p>
+  {/each}
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
+
+  h1 {
+    width: 100%;
+  }
+</style>
